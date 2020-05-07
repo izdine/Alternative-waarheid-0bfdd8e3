9ddded0cef9b418a79396ab8fa70df3809ec2d80
Iz-Dine Amadou <2088346@talnet.nl>
2020-05-07 13:59:17 +0200

    Uitwerking van de exercise

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const antwoorden = {
     Frankrijk : "Parijs",
     spin : "8",
-    meer : "IJsselmeer"
+    meer : "IJsselmeer",
+    automerk : ["Volkswagen", "Audi", "Opel", "Porsche", "BMW", "Mercedes", "Mercedes-Benz"],
+   waddeneiland :["Texel", "Vlieland", "Terschelling", "Ameland", "Schiermonnikoog"]
 };
 
 function check(){
@@ -9,7 +11,7 @@ let allsgoed = true;
 
 Object.keys(antwoorden).forEach(function (item) {
 let element = document.getElementById(item)
-if (element.value === antwoorden[item]) {
+if ( antwoorden[item].includes(element.value)) {
  let input = document.getElementsByTagName("input")
 for (i = 0; i < input.length; i++) {
     input[i].style.backgroundColor = "green";
@@ -26,7 +28,7 @@ document.getElementById("juist").innerText ='😥je hebt een paar fouten😥'
 });
 
 if (allsgoed) {
-  document.getElementById("juist").innerText = "🎊🎉Alle antwoorden zijn correct✌️🎊🎉"
+  document.getElementById("juist").innerText = "🎊🎉Alle antwoorden zijn correct🎊🎉✌️"
   }
 
 }
